afb8f1cef1e5999e5cd423e63e15393500cdacb0
misael <missael133@gmail.com>
2019-03-05 00:49:49 -0600

    Fallas en dda

--- Drawn Of Circle/Form1.Designer.cs
+++ Drawn Of Circle/Form1.Designer.cs
@@ -28,13 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblcoordenadaC = new System.Windows.Forms.Label();
+            this.lblcoordenadaR = new System.Windows.Forms.Label();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.lbltimemidpoint = new System.Windows.Forms.Label();
+            this.lbltimedda = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblradio = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mid Point";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DDA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Coordenadas Centrales";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Coordenadas del radio";
+            // 
+            // lblcoordenadaC
+            // 
+            this.lblcoordenadaC.AutoSize = true;
+            this.lblcoordenadaC.Location = new System.Drawing.Point(566, 139);
+            this.lblcoordenadaC.Name = "lblcoordenadaC";
+            this.lblcoordenadaC.Size = new System.Drawing.Size(52, 13);
+            this.lblcoordenadaC.TabIndex = 5;
+            this.lblcoordenadaC.Text = "( 00 , 00 )";
+            // 
+            // lblcoordenadaR
+            // 
+            this.lblcoordenadaR.AutoSize = true;
+            this.lblcoordenadaR.Location = new System.Drawing.Point(569, 248);
+            this.lblcoordenadaR.Name = "lblcoordenadaR";
+            this.lblcoordenadaR.Size = new System.Drawing.Size(46, 13);
+            this.lblcoordenadaR.TabIndex = 6;
+            this.lblcoordenadaR.Text = "(00 , 00)";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(557, 374);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(97, 23);
+            this.btn_clear.TabIndex = 7;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // lbltimemidpoint
+            // 
+            this.lbltimemidpoint.AutoSize = true;
+            this.lbltimemidpoint.Location = new System.Drawing.Point(115, 36);
+            this.lbltimemidpoint.Name = "lbltimemidpoint";
+            this.lbltimemidpoint.Size = new System.Drawing.Size(40, 13);
+            this.lbltimemidpoint.TabIndex = 8;
+            this.lbltimemidpoint.Text = "00 : 00";
+            // 
+            // lbltimedda
+            // 
+            this.lbltimedda.AutoSize = true;
+            this.lbltimedda.Location = new System.Drawing.Point(323, 36);
+            this.lbltimedda.Name = "lbltimedda";
+            this.lbltimedda.Size = new System.Drawing.Size(40, 13);
+            this.lbltimedda.TabIndex = 9;
+            this.lbltimedda.Text = "00 : 00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(583, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Radio";
+            // 
+            // lblradio
+            // 
+            this.lblradio.AutoSize = true;
+            this.lblradio.Location = new System.Drawing.Point(594, 318);
+            this.lblradio.Name = "lblradio";
+            this.lblradio.Size = new System.Drawing.Size(0, 13);
+            this.lblradio.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(738, 450);
+            this.Controls.Add(this.lblradio);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbltimedda);
+            this.Controls.Add(this.lbltimemidpoint);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.lblcoordenadaR);
+            this.Controls.Add(this.lblcoordenadaC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblcoordenadaC;
+        private System.Windows.Forms.Label lblcoordenadaR;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label lbltimemidpoint;
+        private System.Windows.Forms.Label lbltimedda;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblradio;
     }
 }
 
--- Drawn Of Circle/Form1.cs
+++ Drawn Of Circle/Form1.cs
@@ -7,11 +7,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Drawn_Of_Circle
 {
     public partial class Form1 : Form
     {
+
+        Point Punto_C;
+        Point Punto_R;
+        Bitmap bmp = new Bitmap(480, 360);
+
+        double r = 0;
+        int xc, yc;
+        int xr, yr;
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(Brushes.White, 0, 0, 370, 350);
+            pictureBox1.Image = bmp;
+            lbltimedda.Text = "00 : 00";
+            lbltimemidpoint.Text = "00 : 00";
+            lblcoordenadaC.Text = "(00 , 00)";
+            lblcoordenadaR.Text = "(00 , 00)";
+            lblradio.Text = "0";
+        }
+
+        private void CrearCoordenada()
+        {
+            int xi, yi, xf, yf;
+            Pen pen = new Pen(Color.Tomato, 3);
+            Graphics Gfx = Graphics.FromImage(bmp);
+
+            xi = Punto_C.X;
+            yi = Punto_C.Y;
+
+            xf = Punto_R.X;
+            yf = Punto_R.Y;
+
+            Gfx.FillRectangle(Brushes.Tomato, xi - 1, yi - 1, 2, 2);
+            Gfx.FillRectangle(Brushes.Tomato, xf - 1, yf - 1, 2, 2);
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (xc == 0)
+            {
+                xc = e.X;
+                yc = e.Y;
+
+                Punto_C = new Point((int)xc, (int)yc);
+            }
+            else
+            {
+                xr = e.X;
+                yr = e.Y;
+
+                Punto_R = new Point((int)xr, (int)yr);
+
+                r = Math.Round(Math.Sqrt(Math.Pow(xr - xc, 2) + Math.Pow(yr - yc, 2)));
+
+
+                lblcoordenadaC.Text = "(" + xc + " , " + yc + ")";
+                lblcoordenadaR.Text = "(" + xr + " , " + yr + ")";
+                lblradio.Text = r.ToString();
+
+                CirculoDDA(xc,yc, r, xr,yr);
+                CrearCoordenada();
+
+                pictureBox1.Image = bmp;
+               
+                xc = 0;
+
+            }
+        }
+
+        private double Radio(int xc, int yc, int xr, int yr)
+        {
+            double r = 0;
+            r = Math.Round(Math.Sqrt(Math.Pow((double)xr - (double)xc, 2) + Math.Pow((double)yr - (double)yc, 2)));
+            return Math.Abs(r);
+        }
+
+        private void CirculoDDA(int xc, int yc, double r, int Xr, int yr )
+        {
+              double yk=10;
+            
+            //Console.WriteLine(xc + yc);
+            for (int xk =0; xk < yk; xk++)
+            {
+                yk = Math.Sqrt(Math.Pow(r, 2) - Math.Pow(xk, 2));
+                /*
+                bmp.SetPixel((int)yk + xc  ,  xk+yc, Color.Red);
+                bmp.SetPixel( xk + xc  ,  (int)yk + yc, Color.Red);
+                bmp.SetPixel(-xk + xc  ,  (int)yk + yc, Color.Red);
+                bmp.SetPixel(-(int)yk + xc  ,  xk + yc, Color.Red);
+                bmp.SetPixel(-(int)yk + xc  ,  xk + yc, Color.Red);
+                bmp.SetPixel(-(int)yk + xc  ,  -(xk) + yc, Color.Red);
+                bmp.SetPixel(xk + xc  ,  -(int)yk + yc, Color.Red);
+                bmp.SetPixel((int)yk + xc  ,  -(xk) + yc, Color.Red);
+                */
+            }
+        }
+            
+        private void Midpoint(int xc, int yc, int r){
+
+            int x = r;
+            int y = 0;
+
+            Console.WriteLine("("+ x + xc + "," + y + yc +")");
+
+            if (r > 0)
+            {
+                Console.WriteLine("(" + x + xc + "," + -y + yc + ")");
+                Console.WriteLine("(" + x + xc + "," + y + yc + ")");
+
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
